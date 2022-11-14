17e18c7b2e2889ce1564e94604a0013337539f04
backendpython <holmirzaevsohruh196@gmail.com>
2022-11-14 15:05:19 +0500

    main

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const canvas = document.getElementById('#canvas1')
-const ctx = canvas.getContext('2d')
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 400;
 
@@ -10,7 +10,24 @@ let frame = 0;
 let score = 0;
 let gamespeed = 2;
 
+let temp = canvas.height - 90
+
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(10, temp, 50, 50);
     requestAnimationFrame(animate)
 }
+animate()
+
+window.addEventListener('keydown', function (e) {
+    if (e.code=='Space') {
+        spacePressed = true;
+        temp -= 20
+    }
+})
+
+window.addEventListener('keyup', function (e) {
+    if (e.code=='Space') {
+        spacePressed = false;
+    }
+})
