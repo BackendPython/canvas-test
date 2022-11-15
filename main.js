2c0565645e02b3a4b5bb1dac99ccc45d57ab750b
backendpython <holmirzaevsohruh196@gmail.com>
2022-11-15 17:19:06 +0500

    create game over

--- main.js
+++ main.js
@@ -14,12 +14,16 @@ let gamespeed = 2;
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
+    handleObstacles()
+    handleParticles();
     bird.uptade();
     bird.draw();
-    handleParticles();
+    handleColisions();
+    if (handleColisions()) return;
     requestAnimationFrame(animate);
     angle+=0.12;
     hue++;
+    frame++;
 }
 
 animate()
@@ -35,3 +39,24 @@ window.addEventListener('keyup', function (e) {
         spacePressed = false;
     }
 })
+
+const bang = new Image();
+bang.src = 'images/bang.png';
+bang.alt = 'none';
+
+function handleColisions() {
+    for (let i = 0; i < obstacleArray.length; i++) {
+        if (bird.x < obstacleArray[i].x + obstacleArray[i].width &&
+            bird.x + bird.width > obstacleArray[i].x &&
+            ((bird.y < 0 + obstacleArray[i].top && bird.y + bird.height > 0) ||
+            (bird.y > canvas.height - obstacleArray[i].bottom &&
+                bird.y + bird.height < canvas.height))) {
+            ctx.drawImage(bang, bird.x, bird.y, 50, 50);
+            ctx.font = '25px Georgia';
+            ctx.fillStyle = 'black';
+            ctx.fillText('Game Over, your score is ' + score, 160, canvas.height/2 -10);
+            
+            return true;
+        }
+    }
+}
--- obstacles.js
+++ obstacles.js
@@ -3,14 +3,14 @@ const obstacleArray = []
 class Obstacle {
     constructor(){
         this.top = (Math.random() * canvas.height/3) + 20;
-        this.bottom = (Math.random() * caches.height/3) + 20;
-        this.x = caches.width;
+        this.bottom = (Math.random() * canvas.height/3) + 20;
+        this.x = canvas.width;
         this.width = 20;
-        this.color = 'hsl(' + hue + ', 100%, 50%)';
+        this.color = 'hsla(' + hue + ', 100%, 50%, 1)';
     }
     draw(){
         ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.width, this.top);
+        ctx.fillRect(this.x, 0, this.width, this.top);
         ctx.fillRect(this.x, canvas.height - this.bottom, this.width, this.bottom);
     }
     uptade(){
@@ -19,3 +19,14 @@ class Obstacle {
     }
 }
 
+function handleObstacles() {
+    if (frame%50===0) {
+        obstacleArray.unshift(new Obstacle);
+    }
+    for(let i = 0; i < obstacleArray.length; i++){
+        obstacleArray[i].uptade()
+    }
+    if (obstacleArray.length > 20) {
+        obstacleArray.pop(obstacleArray[0])
+    }
+}
